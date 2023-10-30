fe6fc263c652155b178d79261e528f417d4fa820
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-10-30 07:59:11 -0300

    feat: style view ranking

--- ranking.php
+++ ranking.php
@@ -13,46 +13,46 @@ $conexao->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="navigation.js" defer></script>
+    <link rel="stylesheet" href="ranking.css">
     <title>Chaninhas Game | Ranking</title>
 </head>
 
 <body>
-    <header>
-        <button id="btn-home">< Home</button>
-        <h1 class="title">Ranking</h1>
-    </header>
     <main>
-        <section>
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>NOME</th>
-                        <th>ACERTOS</th>
-                        <th>ERROS</th>
-                        <th>DATA/HORA</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $consulta = $conexao->query("SELECT * FROM partidas;");
-                    $partidas = $consulta->fetchAll();
+        <header>
+            <button id="btn-home">
+                < Home</button>
+                    <h1 class="title">Ranking</h1>
+        </header>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>NOME</th>
+                    <th>ACERTOS</th>
+                    <th>ERROS</th>
+                    <th>DATA/HORA</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $consulta = $conexao->query("SELECT * FROM partidas;");
+                $partidas = $consulta->fetchAll();
 
-                    foreach ($partidas as $p) :
-                    ?>
-                        <tr>
-                            <td><?= $p->id ?></td>
-                            <td><?= $p->nome ?></td>
-                            <td><?= $p->qntdAcertos ?></td>
-                            <td><?= $p->qntdErros ?></td>
-                            <td><?= $p->dataehora ?></td>
-                        </tr>
-                    <?php
-                    endforeach;
-                    ?>
-                </tbody>
-            </table>
-        </section>
+                foreach ($partidas as $p) :
+                ?>
+                    <tr>
+                        <td><?= $p->id ?></td>
+                        <td><?= $p->nome ?></td>
+                        <td><?= $p->qntdAcertos ?></td>
+                        <td><?= $p->qntdErros ?></td>
+                        <td><?= $p->dataehora ?></td>
+                    </tr>
+                <?php
+                endforeach;
+                ?>
+            </tbody>
+        </table>
     </main>
 </body>
 
